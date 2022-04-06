87ce0773064354bcc6340d81f766e7decca20666
Alexey Gorlenko <a.gorlenko@hh.ru>
2022-04-06 13:20:09 +0300

    HH-148247 review fixes

---
+++ docker/base/build_base.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#./build_base.sh 1.0.0
+set -euo pipefail
+
+tag="giftless-base-$1"
+
+full_tag="registry.pyn.ru/giftless-base:$tag"
+DOCKER_BUILDKIT=1 docker build --ssh default --progress plain --no-cache --tag "$full_tag" ../..
+docker push "$full_tag"
